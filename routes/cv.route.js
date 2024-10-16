8b3055fefdf23053f1b2a5dc7ad565f7656235c7
abdul-kodir2020 <62393812+abdul-kodir2020@users.noreply.github.com>
2024-10-16 17:23:27 +0200

    Update cv.route.js

--- routes/cv.route.js
+++ routes/cv.route.js
@@ -1,9 +1,10 @@
 const upload = require('../middlewares/upload.middleware')
 const verifyToken = require('../middlewares/auth.middleware')
-const { getCv, getCvs, addCv, updateCv } = require('../controllers/cv.controller')
+const { getCv, getCvs, addCv, updateCv, getCvByUserId} = require('../controllers/cv.controller')
 const router = require('express').Router()
 
 router.get('/:id',verifyToken ,getCv)
+router.get('/user/me',verifyToken ,getCvByUserId)
 router.get('/', getCvs)
 router.post('/',verifyToken ,upload.single('image'), addCv)
 router.put('/:id',verifyToken ,upload.single('image'), updateCv)
